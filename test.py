4bbd556cfff5865c5bb3fa2580c7a1c2be52e8a9
technnova <technnova@users.noreply.github.com>
2022-06-16 17:47:10 +0000

    autopep8 action fixes

--- test.py
+++ test.py
@@ -3,12 +3,13 @@ import boto3
 ec = boto3.client('ec2', region_name='us-west-2')
 elb = boto3.client('elbv2', region_name='us-west-2')
 
+
 def lambda_handler(event, context):
 
     reservations = ec.describe_instances(
         Filters=[
             {'Name': 'tag:Automated', 'Values': ['HA']},
-            { 'Name': 'instance-state-name','Values': ['running'], }
+            {'Name': 'instance-state-name', 'Values': ['running'], }
         ]).get('Reservations', [])
 
     instances = sum(
@@ -18,27 +19,21 @@ def lambda_handler(event, context):
     for instance in instances:
 
         try:
-            stack_tag = [t.get('Value') for t in instance['Tags'] if t['Key'] == 'aws:cloudformation:stack-name'][0]
+            stack_tag = [t.get('Value') for t in instance['Tags']
+                         if t['Key'] == 'aws:cloudformation:stack-name'][0]
         except IndexError:
-            stack_tag = [t.get('Value') for t in instance['Tags'] if t['Key'] == 'Stack'][0]
+            stack_tag = [t.get('Value')
+                         for t in instance['Tags'] if t['Key'] == 'Stack'][0]
 
-        target_groups = elb.describe_target_groups() 
-        target_group = [tg['TargetGroupArn'] for tg in target_groups['TargetGroups'] if stack_tag[:3] in tg['TargetGroupArn']]    
+        target_groups = elb.describe_target_groups()
+        target_group = [tg['TargetGroupArn']
+                        for tg in target_groups['TargetGroups'] if stack_tag[:3] in tg['TargetGroupArn']]
 
         elb.register_targets(
             TargetGroupArn=target_group[0],
             Targets=[
                 {
-                    'Id' : instance['InstanceId']
+                    'Id': instance['InstanceId']
                 }
             ]
         )
-
-
-
-
-
-
-
-
-
